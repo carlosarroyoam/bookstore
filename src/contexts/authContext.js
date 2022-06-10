7dd1f16bc45533079f94a3969f75345b3a82d043
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-06-10 17:43:22 -0500

    add isAuthenticated to AuthContext

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,32 +1,48 @@
 import Router from 'next/router';
-import { parseCookies } from 'nookies';
-import { createContext } from 'react';
+import { parseCookies, setCookie } from 'nookies';
+import { createContext, useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { login, logout } from '../services/auth.service';
 
 export const AuthContext = createContext({});
 
-export function AuthProvider({ children }) {
-  async function logIn({ email, password }) {
-    const { device_fingerprint } = parseCookies();
+export function AuthProvider({ authenticated, children }) {
+  const [deviceFingerprint, setDevicefingerprint] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(authenticated);
 
+  useEffect(() => {
+    if (!parseCookies().device_fingerprint) {
+      setCookie(undefined, 'device_fingerprint', uuidv4());
+    }
+
+    setDevicefingerprint(parseCookies().device_fingerprint);
+  }, []);
+
+  async function logIn({ email, password }) {
     await login({
       email,
       password,
-      device_fingerprint,
+      device_fingerprint: deviceFingerprint,
     });
+
+    setIsAuthenticated(true);
 
     Router.push('/users');
   }
 
   async function logOut() {
-    const { device_fingerprint } = parseCookies();
-
     await logout({
-      device_fingerprint,
+      device_fingerprint: deviceFingerprint,
     });
+
+    setIsAuthenticated(false);
 
     Router.push('/login');
   }
 
-  return <AuthContext.Provider value={{ logIn, logOut }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ logIn, logOut, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,15 @@
-import { parseCookies, setCookie } from 'nookies';
-import { v4 as uuidv4 } from 'uuid';
+import App from 'next/app';
+import { parseCookies } from 'nookies';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import Main from '../components/main/Main';
 import { AuthProvider } from '../contexts/AuthContext';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  const { device_fingerprint } = parseCookies();
-  if (!device_fingerprint) setCookie(undefined, 'device_fingerprint', uuidv4());
-
+function MyApp({ Component, pageProps, authenticated }) {
   return (
     <>
-      <AuthProvider>
+      <AuthProvider authenticated={authenticated}>
         <Header />
 
         <Main>
@@ -24,5 +21,19 @@ function MyApp({ Component, pageProps }) {
     </>
   );
 }
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+MyApp.getInitialProps = async (appContext) => {
+  const { refresh_token } = parseCookies(appContext.ctx);
+
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  return { authenticated: !!refresh_token, ...appProps };
+};
 
 export default MyApp;
