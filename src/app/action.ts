41ed55d0b7fdc17ca6fb9cf423e4a04e75112b35
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-11-02 21:31:50 -0600

    Elimina action.ts

--- src/app/action.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { cookies } from "next/headers";
-
-export async function getSession() {
-  const isAuth = cookies().has("refresh_token");
-
-  return { isAuth };
-}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
-export default function AuthLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
